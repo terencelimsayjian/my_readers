f420f2fc561da687c1b4d633228a09970bf4019b
terence lim <terencelim92@gmail.com>
2017-07-14 14:58:57 +0800

    Add devise for admin login with username. Configured bootstrap.

---
+++ db/migrate/20170717035757_devise_create_admins.rb
@@ -0,0 +1,18 @@
+class DeviseCreateAdmins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :admins do |t|
+      ## Database authenticatable
+      t.string :username,           null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  # TODO: Add tests for redirection when dashboard views have been implemented
+end
