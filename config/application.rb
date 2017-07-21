f2a2d3141a0d32050847bc5683354210d5924fc8
terence lim <terencelim92@gmail.com>
2017-07-21 14:34:49 +0800

    Add config to remove auto generation of stylesheets, javascripts, and helpers. Generated project controller.

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,9 @@ module MyReaders
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
+    config.generators.helper = false
   end
 end
