ee3e97b1c7674df6ccc2162958ad3dafca769f79
terence lim <terencelim92@gmail.com>
2017-07-24 12:01:11 +0800

    Add dynamic class to layout to load specific css files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def css_class_for_controller_action(controller)
+    "#{controller.controller_path.parameterize.tr('-', '_')} #{controller.action_name}"
+  end
+
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,2 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Add name of controlelrs to array, and create a stylesheet nested in the folder of the same name
-%w(static_pages).each do |controller|
-  Rails.application.config.assets.precompile += ["#{controller}/#{controller}"]
-end
