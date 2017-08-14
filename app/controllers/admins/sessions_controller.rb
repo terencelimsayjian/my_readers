7ac54beadbf6e627c5e18a69c9e036580b881765
terence lim <terencelim92@gmail.com>
2017-08-14 14:27:11 +0800

    Styled admin login page

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,17 +1,6 @@
 class Admins::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
-
-  # def new
-  #   super
-  # end
-
-  # def create
-  #   super
-  # end
-
-  # def destroy
-  #   super
-  # end
+  layout 'no_nav_bar'
 
   protected
 
