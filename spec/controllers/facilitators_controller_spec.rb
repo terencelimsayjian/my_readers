f09c06579035d6fb34ff0eb1e96900f795113f9e
terence lim <terencelim92@gmail.com>
2017-08-14 18:48:14 +0800

    Abstract form css out into its own stylesheet

--- spec/controllers/facilitators_controller_spec.rb
+++ spec/controllers/facilitators_controller_spec.rb
@@ -28,11 +28,4 @@ RSpec.describe FacilitatorsController, type: :controller do
       end
     end
   end
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/controllers/sessions_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SessionsController, type: :controller do
-  # TODO: Add tests for redirection when dashboard views have been implemented
-end
