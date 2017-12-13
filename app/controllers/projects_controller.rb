483809de5dfbf72e2ecf6689ce80a058bc04c5d7
gohdaniel15 <gohdaniel15@gmail.com>
2017-12-13 22:34:08 +0800

    Add #new and #create actions for Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,29 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_admin
+  before_action :prepare_facilitator, only: [:new, :create]
 
   def new
+    @project = @facilitator.projects.build
   end
 
   def create
+    @project = @facilitator.projects.build(project_params)
+    if @project.save
+      flash[:notice] = 'Project successfully created'
+      redirect_to facilitators_path
+    else
+      flash[:alert] = @project.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
+  private
+
+  def prepare_facilitator
+    @facilitator = Facilitator.find(params[:facilitator_id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :estimated_start_date, :estimated_end_date)
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { create(:project) }
+
+  it { expect(subject).to validate_presence_of(:name) }
+
+  describe 'custom validations' do
+    context 'when end date is later than start date' do
+      it { expect(subject.valid?).to be(true) }
+    end
+
+    context 'when end date is earlier than start date' do
+      let(:subject) { build(:project, :invalid) }
+      it { expect(subject.valid?).to be(false) }
+    end
+  end
 end
