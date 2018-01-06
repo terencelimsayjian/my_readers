10ab7ac4ae258caf04472f06f48e0bae33e75fe7
terence lim <terencelim92@gmail.com>
2018-01-06 15:37:30 +0800

    Refactor input element ID and name methodsˆ

--- app/assets/javascripts/diagnostic_new/diagnostic_new.js
+++ app/assets/javascripts/diagnostic_new/diagnostic_new.js
@@ -1,42 +1,49 @@
+"use strict";
+
 $(document).on('turbolinks:load', function () {
     var startingNumberOfRows = $(".diagnostic-form-container > .new_diagnostic > .input-row-container > div.input-row").length;
     for (var i = 0; i < startingNumberOfRows; i++) {
         assignScoreCalculationListener(i);
     }
 
-    $(".clickable-link-container > a.add-next-level").click(function () {
-        var levelsData = $("#levels_information").data().levelsInformation;
-        var inputRowIndex = $("div.input-row").length;
+    $(".clickable-link-container > a.add-next-level").click(addNextInputRow);
 
-        var numberOfWords = levelsData[inputRowIndex]
-        $("div.input-row-container").append(diagnosticInputRow(inputRowIndex, numberOfWords));
-
-        assignScoreCalculationListener(inputRowIndex);
-    });
-
-    $(".clickable-link-container > a.remove-latest-level").click(function () {
-        var numberOfInputRows = $("div.input-row").length;
-        if (numberOfInputRows > 1) {
-            $("div.input-row").last().remove();
-        }
-    });
+    $(".clickable-link-container > a.remove-latest-level").click(removeLastInputRow);
 
     $(".submit-new-diagnostic").click(performValidations);
 });
+
+function addNextInputRow() {
+    var levelsData = $("#levels_information").data().levelsInformation;
+    var inputRowIndex = $("div.input-row").length;
+
+    var numberOfWords = levelsData[inputRowIndex];
+    $("div.input-row-container").append(diagnosticInputRow(inputRowIndex, numberOfWords));
+
+    assignScoreCalculationListener(inputRowIndex);
+}
+
+function removeLastInputRow() {
+    var numberOfInputRows = $("div.input-row").length;
+    if (numberOfInputRows > 1) {
+        $("div.input-row").last().remove();
+    }
+}
 
 function performValidations() {
     var numberOfInputRows = $("div.input-row").length;
     for (var i = 0; i < numberOfInputRows; i++) {
         (function () {
-            var phonicsScoreInput = document.getElementById('diagnostic_levels_attributes_' + i + '_phonics_score');
-            var numberOfTestedWords = diagnosticsLevelJqueryElement('number_of_tested_words', i).val();
-            var phonicsScore = diagnosticsLevelJqueryElement('phonics_score', i).val();
+            var phonicsScoreInput = document.getElementById(levelAttributeInputID('phonics_score', i));
+            var numberOfTestedWords = levelAttributeInput('number_of_tested_words', i).val();
+            var phonicsScore = levelAttributeInput('phonics_score', i).val();
 
             if (!phonicsScoreLessThanTestedWords(phonicsScore, numberOfTestedWords)) {
                 phonicsScoreInput.setCustomValidity("Phonics score must be less than total number of words.");
             }
 
-            if (!phonicsScorePassesThreshold(phonicsScore, numberOfTestedWords) && (i < numberOfInputRows - 1)) {
+            var earlierLevelsPassScoreThreshold = !phonicsScorePassesThreshold(phonicsScore, numberOfTestedWords) && (i < numberOfInputRows - 1);
+            if (earlierLevelsPassScoreThreshold) {
                 phonicsScoreInput.setCustomValidity("Phonics score must be 99% or higher before proceeding to next level.");
             }
         })();
@@ -52,66 +59,10 @@ function phonicsScorePassesThreshold(phonicsScore, numberOfTestedWords) {
     return roundedPercentageScore >= 0.99;
 }
 
-function diagnosticInputRow(inputRowIndex, numberOfTestedWords) {
-    return $(
-        '<div class="row-container input-row">' +
-            hiddenDiagnosticAttribute('reading_level', inputRowIndex) +
-            '<div class="diagnostic-form-group">' +
-                '<input type="hidden" value=' + numberOfTestedWords + ' class="diagnostic-form-input" name=' + diagnosticAttributeName('number_of_tested_words', inputRowIndex) + 'id=' + diagnosticAttributeId('number_of_tested_words', inputRowIndex) + '>' +
-                '<p>' + numberOfTestedWords + '</p>' +
-            '</div>' +
-            diagnosticAttribute('phonics_score', inputRowIndex, 'phonics-score') +
-            '<div class="diagnostic-form-group">' +
-                '<p class="percentage-words-recognised"></p>' +
-            '</div>' +
-            diagnosticAttribute('fluency_score', inputRowIndex) +
-            diagnosticAttribute('comprehension_score', inputRowIndex) +
-        '</div>'
-    );
-}
-
-function hiddenDiagnosticAttribute(attribute, inputRowIndex) {
-    var attributeName = diagnosticAttributeName(attribute, inputRowIndex);
-    var attributeId = diagnosticAttributeId(attribute, inputRowIndex);
-
-    return '<div class="diagnostic-form-group">' +
-                '<input type="hidden" value=' + (inputRowIndex + 1) + ' class="diagnostic-form-input" name=' + attributeName + 'id=' + attributeId + '>' +
-                '<p>' + (inputRowIndex + 1) + '</p>' +
-            '</div>';
-}
-
-function diagnosticAttribute(attribute, inputRowIndex, extraClass) {
-    var attributeName = diagnosticAttributeName(attribute, inputRowIndex);
-    var attributeId = diagnosticAttributeId(attribute, inputRowIndex);
-    var classes = extraClass ? ['diagnostic-form-group', extraClass] : ['diagnostic-form-group'];
-
-    return divWithClasses(classes) +
-            '<input type="number" class="diagnostic-form-input" name=' + attributeName + 'id=' + attributeId + ' required>' +
-        '</div>';
-}
-
-function divWithClasses(classes) {
-    return '<div class="' + classes.join(' ') + '">';
-}
-
-function diagnosticAttributeName(attribute, inputRowIndex) {
-    var name = "diagnostic[levels_attributes][" + inputRowIndex + "][" + attribute + "]";
-    return '"' + name + '"';
-}
-
-function diagnosticAttributeId(attribute, inputRowIndex) {
-    var id =  'diagnostic_levels_attributes_' + inputRowIndex + '_' + attribute;
-    return '"' + id + '"';
-}
-
-function diagnosticsLevelJqueryElement(attribute, inputRowIndex) {
-    return $('#diagnostic_levels_attributes_' + inputRowIndex + '_' + attribute);
-}
-
 function assignScoreCalculationListener(inputRowIndex) {
-    var numberOfTestedWords = diagnosticsLevelJqueryElement('number_of_tested_words', inputRowIndex).val();
-    var phonicsScoreJqueryInput = diagnosticsLevelJqueryElement('phonics_score', inputRowIndex);
-    var phonicsScoreHtmlInput = document.getElementById('diagnostic_levels_attributes_' + inputRowIndex + '_phonics_score');
+    var numberOfTestedWords = levelAttributeInput('number_of_tested_words', inputRowIndex).val();
+    var phonicsScoreJqueryInput = levelAttributeInput('phonics_score', inputRowIndex);
+    var phonicsScoreHtmlInput = document.getElementById(levelAttributeInputID('phonics_score', inputRowIndex));
 
     phonicsScoreJqueryInput.keyup(function () {
         phonicsScoreHtmlInput.setCustomValidity("");
@@ -123,6 +74,53 @@ function assignScoreCalculationListener(inputRowIndex) {
     });
 }
 
+function diagnosticInputRow(inputRowIndex, numberOfTestedWords) {
+    return $(
+        '<div class="row-container input-row">' +
+            hiddenDiagnosticAttribute('reading_level', inputRowIndex, inputRowIndex + 1) +
+            hiddenDiagnosticAttribute('number_of_tested_words', inputRowIndex, numberOfTestedWords) +
+            diagnosticAttribute('phonics_score', inputRowIndex, 'phonics-score') +
+            '<div class="diagnostic-form-group">' +
+                '<p class="percentage-words-recognised"></p>' +
+            '</div>' +
+            diagnosticAttribute('fluency_score', inputRowIndex) +
+            diagnosticAttribute('comprehension_score', inputRowIndex) +
+        '</div>'
+    );
+}
+
+function hiddenDiagnosticAttribute(attribute, inputRowIndex, inputValue) {
+    var attributeName = levelAttributeInputName(attribute, inputRowIndex);
+    var attributeId = levelAttributeInputID(attribute, inputRowIndex);
+
+    return '<div class="diagnostic-form-group">' +
+                '<input type="hidden" value=' + inputValue + ' class="diagnostic-form-input" name="' + attributeName + '" id="' + attributeId + '">' +
+                '<p>' + inputValue + '</p>' +
+            '</div>';
+}
+
+function diagnosticAttribute(attribute, inputRowIndex, extraClass) {
+    var attributeName = levelAttributeInputName(attribute, inputRowIndex);
+    var attributeId = levelAttributeInputID(attribute, inputRowIndex);
+    var classes = extraClass ? ['diagnostic-form-group', extraClass] : ['diagnostic-form-group'];
+
+    return '<div class="' + classes.join(' ') + '">' +
+                '<input type="number" class="diagnostic-form-input" name="' + attributeName + '" id="' + attributeId + '" required>' +
+            '</div>';
+}
+
 function calculatePercentage(numerator, denominator) {
     return Math.round((numerator / denominator) * 100);
+}
+
+function levelAttributeInputName(attribute, inputRowIndex) {
+    return "diagnostic[levels_attributes][" + inputRowIndex + "][" + attribute + "]";
+}
+
+function levelAttributeInputID(attribute, inputRowIndex) {
+    return  'diagnostic_levels_attributes_' + inputRowIndex + '_' + attribute;
+}
+
+function levelAttributeInput(attribute, inputRowIndex) {
+    return $('#' + levelAttributeInputID(attribute, inputRowIndex));
 }
